f063e42efb48869477a80c63486f3c4fae7a5927
Dylan Smith - Sterling Systems Dylans Controls Inc <dsmith@sterlingcontrols.com>
2025-04-28 15:52:50 -0500

    bst php

---
+++ bst/bst.php
@@ -0,0 +1,67 @@
+<?php
+
+class Node 
+{
+    public int $value;
+    public ?Node $left = null;
+    public ?Node $right= null;
+
+    public function __construct(int $value)
+    {
+        $this->value = $value;
+    }
+}
+
+class BST 
+{
+    public ?Node $head = null;
+
+    public function addNode(int $value, ?Node $currentNode = null): void
+    {
+        if ($this->head == null)
+        {
+            $this->head = new Node($value);
+            return;
+        } 
+
+        if( $currentNode == null){
+            $currentNode = $this->head;
+        } 
+
+        if ( $value > $currentNode->value )
+        {
+            if( $currentNode->right == null) $currentNode->right = new Node($value);
+            
+            else $this->addNode($value, $currentNode->right);
+        } 
+        elseif ( $value < $currentNode->value)
+        {
+            if( $currentNode->left == null ) $currentNode->left = new Node($value);
+
+            else $this->addNode($value, $currentNode->left);
+        }
+    }
+
+    public function removeNode(): void
+    {
+
+    }
+
+    public function inOrder(): void
+    {
+
+    }
+}
+
+
+$bst = new BST();
+
+$bst->addNode(4);
+$bst->addNode(8);
+$bst->addNode(3);
+$bst->addNode(10);
+$bst->addNode(11);
+$bst->addNode(1);
+
+echo $bst->head->value;
+echo $bst->head->right->value;
